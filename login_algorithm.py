09941d4786bfe8c04ef1ef2b0580e6bcb86505d6
James Greensmith <jgreensmith13@gmail.com>
2023-07-20 21:09:37 +0100

    lock account on 3 or more failed attempts

--- login_algorithm.py
+++ login_algorithm.py
@@ -5,28 +5,35 @@ hostname = socket.gethostname()
 # getting the IP address using socket.gethostbyname() method
 ip_address = socket.gethostbyname(hostname)
 
+# read file and check how many failed logins there are
 with open("login_attempts.txt", "r") as file:
-    print(file.read())
-
-username = input("please enter username: ")
-password_input = input("Please enter your password: ")
+    failed_attempts = file.read().count("Failed")
 
 
+# hard coded credentials
 authorised_user = "thanos1"
 password = "iaminevitable"
 status = ""
 
-# check if credentials are correct
-
-if username != authorised_user or password_input != password:
-    print("incorrect credentials")
-    status = " Failed login attempt"
+# lock access if reach 3 failed login attempts
+if failed_attempts >= 3:
+    print("you've been locked out!")
 else:
-    print("welcome", username)
-    status = " Successful login"
+    # login form
+    username = input("please enter username: ")
+    password_input = input("Please enter your password: ")
 
-with open("login_attempts.txt", "a") as file:
-    file.write(ip_address)
-    file.write(" " + username)
-    file.write(status)
-    file.write("\n")
+    # check if credentials are correct
+    if username != authorised_user or password_input != password:
+        print("incorrect credentials")
+        status = " Failed login attempt"
+    else:
+        print("welcome", username)
+        status = " Successful login"
+
+    # append file with new authorisation log entry
+    with open("login_attempts.txt", "a") as file:
+        file.write(ip_address)
+        file.write(" " + username)
+        file.write(status)
+        file.write("\n")
